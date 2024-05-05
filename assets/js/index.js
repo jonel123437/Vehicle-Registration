a76d1d1fb8ff03bf1ba6e0699bb98a3128a2f2de
jonel123437 <145882424+jonel123437@users.noreply.github.com>
2024-05-05 22:44:31 +0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ document.querySelector('.loginButton').addEventListener('click', function(){
     var email = document.getElementById('email').value.toLowerCase();
     var password = document.getElementById('password').value.toLowerCase();
     if (email == "admin" && password == "admin"){
-        window.location.href = "../../pagesHTML/admin.html";
+        window.location.href = "../pagesHTML/admin.html";
         alert('Welcome, Admin!')
     }
 });
